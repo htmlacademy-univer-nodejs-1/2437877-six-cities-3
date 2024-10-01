949fd74f8b23566d2eb2030ef5d5eed956648da4
Евгений <11@11.11>
2024-10-01 23:10:41 +0500

    сохранялка моков в файлик

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import {Command} from 'commander';
 import {projectVersion} from './version.js';
 import {parseTsvToRentalOffers} from './tsvRentalOffersParser.js';
 import chalk from 'chalk';
+import {RentalOffer} from './domain/rent/RentalOffer.js';
+import axios from 'axios';
+import * as fs from 'node:fs';
 const program = new Command();
 
 program
@@ -12,8 +15,55 @@ program
   .helpOption('--help', chalk.red('печатает этот текст'))
   .helpCommand(false);
 
+
+async function generateData(n: number, url: string): Promise<RentalOffer[]> {
+  const generatedData: RentalOffer[] = [];
+
+  for (let i = 0; i < n; i++) {
+    const response = await axios.get<RentalOffer>(`${url}/${i}`);
+    const data = response.data;
+    data.publishDate = new Date(data.publishDate);
+    data.price = Math.floor(Math.random() * 1000);
+    generatedData.push(data);
+  }
+
+  return generatedData;
+}
+
+function saveDataToFile(data: RentalOffer[], filepath: string) {
+  const tsvData = data.map((offer) => [
+    offer.title,
+    offer.description,
+    offer.publishDate.toISOString(),
+    offer.city,
+    offer.previewImage,
+    offer.photos.join(','),
+    offer.isPremium,
+    offer.isFavorite,
+    offer.rating,
+    offer.housingType,
+    offer.rooms,
+    offer.guests,
+    offer.price,
+    offer.facilities.join(','),
+    offer.author.name,
+    offer.commentsCount,
+    offer.coordinates.join(',')
+  ].join('\t')).join('\n');
+
+  fs.writeFileSync(filepath, tsvData);
+}
+
+
 program
-  .command('--generate <n> <filepath> <url>', 'генерирует произвольное количество тестовых данных');
+  .command('generate')
+  .argument('<n>','')
+  .argument('<filepath>','')
+  .argument('<url>','')
+  .action(async (n:number, filepath:string, url:string)=>{
+    const data = await generateData(n, url);
+    saveDataToFile(data, filepath);
+  });
 
 program
   .command('import')
