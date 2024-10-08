3f5c119507a316c4f9c3b4cc7bedb62fb5cc725a
Евгений <11@11.11>
2024-10-08 23:56:03 +0500

    add envs

--- src/Application.ts
+++ src/Application.ts
@@ -1,10 +1,10 @@
 import {logger} from './infrastructure/Logger.js';
-import {Environments} from "./infrastructure/Environments.js";
+import {Environments} from './infrastructure/Environments.js';
 
 export class Application {
   public Init(){
     logger.info('App started');
-    logger.info(`Server started on ${Environments.PORT}`);
+    logger.info(`Server started on ${Environments.PORT} ${Environments.IP} ${Environments.SALT}`);
   }
 }
 
--- src/infrastructure/Environments.ts
+++ src/infrastructure/Environments.ts
@@ -1,5 +1,12 @@
 import 'dotenv/config.js';
+import {isIP} from 'is-ip';
 
 export const Environments = {
   PORT: Number(process.env.PORT),
+  IP: isIP(process.env.DBIP ?? '') ? process.env.DBIP : undefined,
+  SALT: process.env.SALT
 };
+
+if(Environments.IP == undefined){
+  throw new Error('Invalid database ip');
+}
