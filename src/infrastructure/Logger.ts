3576604d586490a61bef1d6c57b7a0a9fbe4acfb
Евгений <11@11.11>
2024-10-08 23:19:03 +0500

    add pino

---
+++ src/infrastructure/Logger.ts
@@ -0,0 +1,29 @@
+import pino from 'pino';
+
+interface Logger {
+  fatal: pino.LogFn;
+  error: pino.LogFn;
+  warn: pino.LogFn;
+  info: pino.LogFn;
+  debug: pino.LogFn;
+  trace: pino.LogFn;
+}
+
+export const logger : Logger = pino.pino(
+  {
+    level: process.env.PINO_LOG_LEVEL || 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true
+      }
+    }
+  }
+);
+
+
+process.on('uncaughtException', (err) => {
+  logger.fatal(err, 'обнаружено неотловленное исключение');
+  // eslint-disable-next-line node/no-process-exit,no-process-exit
+  process.exit(1);
+});
