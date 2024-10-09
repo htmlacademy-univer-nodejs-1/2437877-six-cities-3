6bed5733bcc8b7a8956dc5ea5d444ae11851b44c
Евгений <11@11.11>
2024-10-09 23:39:55 +0500

    притащил контейнер

---
+++ src/infrastructure/Config/Config.ts
@@ -0,0 +1,60 @@
+import convict from 'convict';
+import validator from 'convict-format-with-validator';
+import { IConfig } from './IConfig.js';
+import {injectable} from "inversify";
+
+convict.addFormats(validator);
+
+@injectable()
+export class Config implements IConfig{
+  innerConfig: convict.Config<IConfig>;
+
+  constructor() {
+    this.innerConfig = convict<IConfig>({
+      env: {
+        doc: 'The application environment.',
+        format: ['production', 'development'],
+        default: 'development',
+        env: 'NODE_ENV'
+      },
+      port: {
+        doc: 'The port to bind.',
+        format: 'port',
+        default: 5050,
+        env: 'PORT'
+      },
+      dbip: {
+        doc: 'Database IP address.',
+        format: 'ipaddress',
+        default: '127.0.0.1',
+        env: 'DBIP'
+      },
+      salt: {
+        doc: 'Salt for passwords.',
+        format: String,
+        default: 'FIWUIUWUGUOYY423423',
+        env: 'SALT'
+      }
+    });
+
+
+    this.innerConfig.validate({ allowed: 'strict' });
+  }
+
+  get env(): string {
+    return this.innerConfig.get('env');
+  }
+
+  get port(): number {
+    return this.innerConfig.get('port');
+  }
+
+  get dbip(): string {
+    return this.innerConfig.get('dbip');
+  }
+
+  get salt(): string {
+    return this.innerConfig.get('salt');
+
+}
+}
---
+++ src/infrastructure/Logger/PinoLogger.ts
@@ -0,0 +1,37 @@
+import { pino } from 'pino';
+import { injectable } from 'inversify';
+import { ILogger } from './ILogger.js';
+
+@injectable()
+export class PinoLogger implements ILogger {
+  private readonly logger;
+
+  constructor() {
+    this.logger = pino();
+
+    process.on('uncaughtException', (err) => {
+      this.logger.fatal(err, 'обнаружено неотловленное исключение');
+      // eslint-disable-next-line node/no-process-exit,no-process-exit
+      process.exit(1);
+    });
+
+
+    this.logger.info('Logger created…');
+  }
+
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.debug(message, ...args);
+  }
+
+  public error(message: string, error: Error, ...args: unknown[]): void {
+    this.logger.error(error, message, ...args);
+  }
+
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+
+  public warn(message: string, ...args: unknown[]): void {
+    this.logger.warn(message, ...args);
+  }
+}
