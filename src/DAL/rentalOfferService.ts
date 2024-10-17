70f72f00d10caed362228ab75870e3201dbf7ddf
Евгений <11@11.11>
2024-10-17 22:54:44 +0500

    add repositories

---
+++ src/DAL/rentalOfferService.ts
@@ -0,0 +1,13 @@
+import { RentalOfferDbo, IRentalOffer } from './rentalOfferDbo.js';
+import {IRentalOfferService} from './databaseService.js';
+
+export class RentalOfferService implements IRentalOfferService {
+  async findById(id: string): Promise<IRentalOffer | null> {
+    return RentalOfferDbo.findById(id).exec();
+  }
+
+  async create(offer: Partial<IRentalOffer>): Promise<IRentalOffer> {
+    const newOffer = new RentalOfferDbo(offer);
+    return newOffer.save();
+  }
+}
---
+++ src/DAL/userService.ts
@@ -0,0 +1,17 @@
+import { UserDbo, IUser } from './userDbo.js';
+import {IUserService} from './databaseService.js';
+
+export class UserService implements IUserService {
+  async findById(id: string): Promise<IUser | null> {
+    return UserDbo.findById(id).exec();
+  }
+
+  async findByEmail(email: string): Promise<IUser | null> {
+    return UserDbo.findOne({ email }).exec();
+  }
+
+  async create(user: Partial<IUser>): Promise<IUser> {
+    const newUser = new UserDbo(user);
+    return newUser.save();
+  }
+}
