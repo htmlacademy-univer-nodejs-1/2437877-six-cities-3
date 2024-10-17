6ae26148de45a8adcf444f22ac65bbbb597342f7
Евгений <11@11.11>
2024-10-17 23:04:56 +0500

    register in di

--- src/infrastructure/types.ts
+++ src/infrastructure/types.ts
@@ -3,4 +3,8 @@ export const TYPES = {
   Application: Symbol.for('Application'),
   Config: Symbol.for('Config'),
   DatabaseClient: Symbol.for('DatabaseClient'),
+  UserService: Symbol.for('IUserService'),
+  UserDbo: Symbol.for('UserDbo'),
+  RentalOfferService: Symbol.for('IRentalOfferService'),
+  RentalOfferDbo: Symbol.for('RentalOfferDbo'),
 };
--- src/main.rest.ts
+++ src/main.rest.ts
@@ -8,6 +8,9 @@ import {Config} from './infrastructure/Config/Config.js';
 import {IConfig} from './infrastructure/Config/IConfig.js';
 import {DatabaseClient} from './infrastructure/Database/database-client.interface.js';
 import {MongoDatabaseClient} from './infrastructure/Mongo/mongo-database-client.js';
+import {IRentalOfferService, IUserService} from './DAL/databaseService.js';
+import {UserService} from './DAL/userService.js';
+import {RentalOfferService} from './DAL/rentalOfferService.js';
 
 async function bootstrap() {
   const container = new Container();
@@ -15,6 +18,8 @@ async function bootstrap() {
   container.bind<Application>(TYPES.Application).to(Application).inSingletonScope();
   container.bind<IConfig>(TYPES.Config).to(Config).inSingletonScope();
   container.bind<DatabaseClient>(TYPES.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
+  container.bind<IUserService>(TYPES.UserService).to(UserService).inSingletonScope();
+  container.bind<IRentalOfferService>(TYPES.RentalOfferService).to(RentalOfferService).inSingletonScope();
 
   const application = container.get<Application>(TYPES.Application);
   application.Init();
