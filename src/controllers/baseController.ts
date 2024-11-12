946352aec5e776bb3b4fa184780386ee9c4c1212
Евгений <11@11.11>
2024-11-12 21:40:25 +0500

    base controller

---
+++ src/controllers/baseController.ts
@@ -0,0 +1,50 @@
+import { Response } from 'express';
+import { injectable } from 'inversify';
+
+@injectable()
+export abstract class BaseController {
+  protected sendOk<T>(res: Response, data: T): Response {
+    return res.status(200).json({
+      success: true,
+      data
+    });
+  }
+
+  protected sendCreated<T>(res: Response, data: T): Response {
+    return res.status(201).json({
+      success: true,
+      data
+    });
+  }
+
+  protected sendBadRequest(
+    res: Response,
+    errorMessage: string
+  ): Response {
+    return res.status(400).json({
+      success: false,
+      errorMessage
+    });
+  }
+
+  protected sendForbidden(
+    res: Response,
+    message: string = 'Forbidden'
+  ): Response {
+    return res.status(403).json({
+      success: false,
+      message
+    });
+  }
+
+  // Ресурс не найден (Not Found - 404)
+  protected sendNotFound(
+    res: Response,
+    message: string = 'Not Found'
+  ): Response {
+    return res.status(404).json({
+      success: false,
+      message
+    });
+  }
+}
